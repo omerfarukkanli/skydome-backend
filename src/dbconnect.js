1413f4dae38ecf05ffdaffeeebb6fb409b926d3e
Omer Faruk Kanli <kanlomerfaruk@gmail.com>
2023-04-27 16:42:36 +0300

    deneme

--- src/dbconnect.js
+++ src/dbconnect.js
@@ -6,7 +6,6 @@ module.exports = {
     mongoose.set("strictQuery", true);
     mongoose.connect(dbUrl, { useNewUrlParser: true });
     const db = mongoose.connection;
-
     db.on("error", (error) => console.error(error));
     db.once("open", () => console.log("connected to database"));
   },
